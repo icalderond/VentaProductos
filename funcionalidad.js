5c1cecc29f15685e8c295ce2302fc5fc6d21bd7f
Israel Calderon <icalderond@outlook.com>
2015-07-23 12:22:36 -0500

    Prueba commit UI

---
+++ funcionalidad.js
@@ -0,0 +1,10 @@
+var $ =window.jQuery
+var $selProductos=$('#selProductos')
+
+$selProductos.append($("<select name='product' id='product'>
+						<option value='dor'>Doritos</option>
+						<option value='curr'>Churritos</option>
+						<option value='fri'>Fritos</option>
+						<option value='che'>Chetos</option>
+					</select>"))
+
